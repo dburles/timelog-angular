2ffd2bcba22a438fe21bcdb7f8befd82b0e2c382
David Burles <dburles@gmail.com>
2013-05-19 20:18:41 +1000

    added service, bug with config not saving..

--- app.js
+++ app.js
@@ -1,40 +1,58 @@
-app = angular.module('timelog', []);
+angular.module('timelog', [])
+	.config(function($routeProvider) {
+		$routeProvider
+			.when('/', { controller: 'MainCtrl', templateUrl: 'main.html'})
+			.when('/config', { controller: 'MainCtrl', templateUrl: 'config.html'})
+			.otherwise({ redirectTo: '/' });
+	})
+	.service('timelogService', function() {
+		var logs = [];
+		var goal = 6;
 
-app.config(function($routeProvider) {
-	$routeProvider
-		.when('/', { controller: 'MainCtrl', templateUrl: 'main.html'})
-		.when('/config', { controller: 'MainCtrl', templateUrl: 'config.html'})
-		.otherwise({ redirectTo: '/' });
-});
+		return {
+			logs: function() {
+				return logs;
+			},
+			add: function(data) {
+				logs.push(data);
+			},
+			getTotal: function() {
+				var total = 0;
+				_.each(logs, function(log) {
+					total += parseFloat(log.time);
+				});
+				return total;
+			},
+			goal: function() {
+				return goal;
+			},
+			saveGoal: function(goal) {
+				goal = goal;
+				console.log(goal);
+			}
+		}
+	})
+	.filter('backwards', function() {
+		return function(items) {
+			return items.slice().reverse();
+		}
+	})
+	.controller('MainCtrl', ['$scope', 'timelogService', function($scope, timelogService) {
+		$scope.goal = timelogService.goal();
+		$scope.logs = timelogService.logs();
 
-app.filter('backwards', function() {
-	return function(items) {
-		return items.slice().reverse();
-	}
-});
+		$scope.add = function() {
+			timelogService.add({ time: $scope.time, task: $scope.task });
+			$scope.time = "";
+			$scope.task = "";
+			$('#time').focus(); // must be an angular way?
+		}
+		$scope.save = function() {
+			timelogService.saveGoal($scope.goal);
+			alert("Saved.");
+		}
 
-app.controller('MainCtrl', function($scope) {
-	$scope.goal = 6;
-	$scope.logs = [];
-
-	$scope.add = function() {
-		$scope.logs.push({ time: $scope.time, task: $scope.task });
-		$scope.time = "";
-		$scope.task = "";
-		
-	}
-	
-	var get_total = function() {
-		var total = 0;
-		_.each($scope.logs, function(log) {
-			total += parseFloat(log.time);
-		});
-		return total;
-	};
-
-	$scope.total = get_total();
-
-	$scope.remaining = parseFloat($scope.goal) - $scope.total;
-
-});
+		$scope.total = timelogService.getTotal();
+		$scope.remaining = parseFloat($scope.goal) - $scope.total;
+	}]);
 
