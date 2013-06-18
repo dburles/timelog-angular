e961e1bb25513734f5536822f8274f0f4581fe4d
David Burles <dburles@gmail.com>
2013-06-18 23:20:30 +1000

    i think i just made more bugs

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 angular.module('timelog', [])
+
 	.config(function($routeProvider, $locationProvider) {
 		$routeProvider
 			.when('/', { controller: 'MainCtrl', templateUrl: 'main.html'})
@@ -6,8 +7,13 @@ angular.module('timelog', [])
 			.otherwise({ redirectTo: '/' });
 		$locationProvider.html5Mode(true);
 	})
+
 	.service('timelogService', function($http) {
-		var logs = [];
+		// Set some logs for testing
+		var logs = [
+			{ time: 3.5, task: 'Writing some angular' },
+			{ time: 1, task: 'Testing' }
+		];
 		var goal = 6;
 
 		return {
@@ -43,25 +49,37 @@ angular.module('timelog', [])
 			}
 		}
 	})
+
 	.filter('backwards', function() {
 		return function(items) {
 			return items.slice().reverse();
 		}
 	})
+
 	.controller('MainCtrl', ['$scope', 'timelogService', function($scope, timelogService) {
 		$scope.goal = timelogService.goal();
 		$scope.logs = timelogService.logs();
 
 		$scope.add = function() {
 			timelogService.add({ time: $scope.time, task: $scope.task });
+
+			// XXX How do we do things like reset form fields and set focus on a specific field?
 			$scope.time = "";
 			$scope.task = "";
 			$('#time').focus(); // must be an angular way?
 		}
 
+		$scope.remove = function(log) {
+			// XXX I believe this is broken somehow because of the 'reverse' filter
+			$scope.logs.splice(log, 1);
+		}
+
+		// XXX Where should this be called to stay up to date? or should getTotal be within the controller???
 		$scope.total = timelogService.getTotal();
+
 		$scope.remaining = parseFloat($scope.goal) - $scope.total;
 	}])
+
 	.controller('ConfigCtrl', ['$scope', 'timelogService', function($scope, timelogService) {
 		$scope.goal = timelogService.goal();
 
